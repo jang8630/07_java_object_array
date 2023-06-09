f6dab59e7ebae9d5c55a85f7192dab781962d8d1
15_web_JCH <15_web_JCH@DESKTOP-6JISKHI>
2023-06-09 17:06:02 +0900

    수정 완료

--- 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx10.java
+++ 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx10.java
@@ -11,18 +11,10 @@ import java.util.Arrays;
  * 	 - ArrayList 클래스는 데이터가 입력되면 자동으로 크기가 커지고 데이터가 삭제되면 자동으로 크기가 작아진다.
  *   - 중간에 데이터가 삽입되면 데이터가 삽입될 위치부터 모든 데이터가 뒤로 이동되고 중간의 데이터가 제거된 다음 위치부터 모든 데이터가 앞으로 이동한다.
  *   
- *   [ Array]
- * 
- *   - 크기가 고정됨     
- *   - 데이터 타입 고정
- *   
- *   
  *   [ ArrayList 생성 방법 ]
  * 
- *   - 크기가 가변적이다. 데이터 타입 가변적이다.
- *   - method로 제어한다.
  *   ArrayList<Integer> list = new ArrayList<Integer>();     
- *   ArrayList<Integer> list = new ArrayList<>();  // 우변의 제네릭의 데이터형은 생략 가능
+ *   ArrayList<Integer> list = new ArrayList<>();           // 우변의 제네릭의 데이터형은 생략 가능하다.
  *   ArrayList<String> list = new ArrayList<String>();
  *   ArrayList<Product> list = new ArrayList<Product>();
  *   ArrayList<Member> list = new ArrayList<Member>();
@@ -44,7 +36,7 @@ import java.util.Arrays;
  *
  *
  *
- *  [ 제네릭, 예시 : "<Integer>" ]
+ *  [ 제네릭 ]
  *  
  * - 제네릭(generic)이라 부르며 ArrayList에 저장될 데이터 타입을 반드시 클래스로 작성한다.
  * - 기본 자료형 데이터를 저장하는 ArrayList를 만들어야 하는 경우에는 래퍼 클래스를 사용한다.
@@ -57,11 +49,11 @@ public class ObjectArrayEx10 {
 	public static void main(String[] args) {
 		
 		int[] arr = new int[10];
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		System.out.println("arr : " + Arrays.toString(arr)); // arr : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-		System.out.println("list : " + list); // list : [] - 데이터
-		System.out.println("\n===========================\n");
+		ArrayList<Integer> list = new ArrayList<Integer>(); 
+			
+		System.out.println("arr : " + Arrays.toString(arr));
+		System.out.println("list : " + list);
+		System.out.println("\n==================\n");
 		
 		// add(value) : arrayList의 맨뒤에 value를 추가한다.
 		// add(index,value) : arrayList의 index번째에 value를 추가한다.
@@ -71,20 +63,21 @@ public class ObjectArrayEx10 {
 		arr[2] = 30;
 		arr[3] = 40;
 		arr[4] = 50;
-
+		
 		list.add(10);
 		list.add(20);
 		list.add(40);
-		list.add(2,  30);
+		list.add(2 , 30);
 		list.add(50);
 		
-		System.out.println("arr : " + Arrays.toString(arr)); // arr : [10, 20, 30, 40, 50, 0, 0, 0, 0, 0]
-		System.out.println("list : " + list); // list : [10, 20, 30, 40, 50]
-		System.out.println("\n===========================\n");		
+		System.out.println("arr : " + Arrays.toString(arr));
+		System.out.println("list : " + list);
+		System.out.println("\n==================\n");
 		
 		// size() : arrayList의 데이터의 개수를 반환한다.
-		System.out.println("arr 크기 : " + arr.length); // 10
-		System.out.println("list 크기 : " + list.size()); // 5
+		System.out.println("arr 크기 : " + arr.length);
+		System.out.println("list 크기 : " + list.size());
+		System.out.println("\n==================\n");
 		
 		// set(index,value) : ArrayList의 index번째의 값을 value값으로 수정한다.
 		arr[0] = 1000;
@@ -93,13 +86,13 @@ public class ObjectArrayEx10 {
 		
 		list.set(0, 1000);
 		list.set(1, 2000);
-		list.set(2, 3000); // 패턴 암기
+		list.set(2, 3000);
 		
-		System.out.println("arr : " + Arrays.toString(arr)); // arr : [1000, 2000, 3000, 40, 50, 0, 0, 0, 0, 0]
-		System.out.println("list : " + list); // list : [1000, 2000, 3000, 40, 50]
-		System.out.println("\n===========================\n");		
+		System.out.println("arr : " + Arrays.toString(arr));
+		System.out.println("list : " + list);
+		System.out.println("\n==================\n");
 		
-		// remove(index) : ArrayList의 index번째 위치의 값을 삭제한다.(reset)
+		// remove(index) : ArrayList의 index번째 위치의 값을 삭제한다.
 		arr[0] = 0;
 		arr[1] = 0;
 		arr[2] = 0;
@@ -108,27 +101,29 @@ public class ObjectArrayEx10 {
 		list.remove(1);
 		list.remove(0);
 		
-		System.out.println("arr : " + Arrays.toString(arr)); // arr : [0, 0, 0, 40, 50, 0, 0, 0, 0, 0]
-		System.out.println("list : " + list); // list : [40, 50]
-		System.out.println("\n===========================\n");		
-		
-		// get(index) : ArrayList의 index번째의 value를 얻어온다.
+		System.out.println("arr : " + Arrays.toString(arr));
+		System.out.println("list : " + list);
+		System.out.println("\n==================\n");
+	
+		// get(index) : ArrayList의 index번째의 value를 얻어온다. 
 		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i] + " ");  // 0, 0, 0, 40, 50, 0, 0, 0, 0, 0			
-		}
-		System.out.println();
-		
-		for (int i = 0; i < list.size(); i++) {
-			System.out.println(list.get(i) + " "); //40, 50			
+			System.out.print(arr[i] + " ");
 		}
 		System.out.println("\n");
 		
-		arr = null; // 배열은 삭제가 되지 않으며, Link를 삭제
-		list.clear(); // ArrayList의 데이터 삭제
-		// list.add(1000);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i) + " ");
+		}
+		System.out.println("\n");
 		
-		list = null; // ArrayList 자체를 삭제
-		// list.add(1000); // 오류
+		arr = null;
+		list.clear(); // ArrayList의 데이터만 삭제
+		//list.add(1000);
+		
+		list = null;  // ArrayList 자체를 삭제
+		//list = new ArrayList<Integer>();
+		//list.add(1000);
+		
 		
 	}
 
--- 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx10_연습.java
+++ 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx10_연습.java
@@ -1,48 +1,18 @@
 package step7_01.objectArray;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ObjectArrayEx10_연습 {
 
 	public static void main(String[] args) {
-		
-		int[] arr = new int[10];
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		
-		System.out.println("arr : " + Arrays.toString(arr)); // arr : [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-		System.out.println("list : " + list); // list : []
-		System.out.println("\n===========================\n");
-		
-		// add(value) : arrayList의 맨뒤에 value를 추가한다.
-		// add(index,value) : arrayList의 index번째에 value를 추가한다.		
-		arr[0] = 100;
-		arr[1] = 200;
-		arr[2] = 300;
 
-		list.add(100);
-		list.add(200);
-		list.add(400);
-		list.add(2,  250);
+		//ArrayList~~~
+		ArrayList<Integer> test1 = new ArrayList<Integer>();
+		ArrayList<Double> test2 = new ArrayList<Double>();
+		ArrayList<String> test3 = new ArrayList<String>();
+		ArrayList<Character> test4 = new ArrayList<Character>();
+		ArrayList<Boolean> test5 = new ArrayList<Boolean>();
 		
-		System.out.println("arr : " + Arrays.toString(arr)); // arr : [100, 200, 300, 0, 0, 0, 0, 0, 0, 0]
-		System.out.println("list : " + list); // list : [100, 200, 250, 400]
-		System.out.println("\n===========================\n");	
-		
-		// set(index,value) : ArrayList의 index번째의 값을 value값으로 수정한다.
-		arr[0] = 10;
-		arr[1] = 20;
-		arr[2] = 30;
-		
-		list.set(0, 10);
-		list.set(1, 20);
-		list.set(2, 30); // 패턴 암기
-		
-		System.out.println("arr : " + Arrays.toString(arr)); // arr : [10, 20, 30, 0, 0, 0, 0, 0, 0, 0]
-		System.out.println("list : " + list); // list : [10, 20, 30, 400]
-		System.out.println("\n===========================\n");		
-		
-
 	}
 
 }
